565d32c7feff885dc4732cc34854892176b27ba6
jean.zhang <jean.zhang@dbschenker.com>
2020-10-15 13:40:40 +0800

    add java demo

---
+++ java/javabasis/src/main/java/org/example/demo/redis/RedisPoolDemo.java
@@ -0,0 +1,11 @@
+package org.example.demo.redis;
+
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+
+public class RedisPoolDemo {
+    public static void main(String[] args) {
+
+        GenericObjectPoolConfig poolConfig = new GenericObjectPoolConfig();
+
+    }
+}
